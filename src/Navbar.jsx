f14478997f4767f67a6eb8cd6f84b3c48d61ef9c
CopterDoctor <Jordananders@me.com>
2018-07-10 12:58:41 -0700

    destructering boiler plate code

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends React.Component {
+
+  render() {
+    return (
+      <nav class="navbar">
+        <a href="/" class="navbar-brand">Chatty</a>
+      </nav>
+    )
+  }
+}
+export default Navbar;
