f4bb8c62f56b3b004b03d2745a63d57e89d28e21
CopterDoctor <Jordananders@me.com>
2018-07-10 13:40:59 -0700

    more destructuring of boiler plate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar';
+import Navbar from './Navbar.jsx';
+import ChatWindow from './Chat-Window.jsx';
+import Footer from './Footer.jsx';
 
 class App extends Component {
   render() {
@@ -7,21 +9,8 @@ class App extends Component {
       <div>
 
         <Navbar />
-
-        <main class="messages">
-          <div class="message">
-            <span class="message-username">Anonymous1</span>
-            <span class="message-content">I won't be impressed with technology until I can download food.</span>
-          </div>
-          <div class="message system">
-            Anonymous1 changed their name to nomnom.
-          </div>
-        </main>
-
-        <footer class="chatbar">
-          <input class="chatbar-username" placeholder="Your Name (Optional)" />
-          <input class="chatbar-message" placeholder="Type a message and hit ENTER" />
-        </footer>
+        <ChatWindow />
+        <Footer />
 
       </div>
     );
